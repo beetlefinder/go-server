a9a984fdfcfcb43a2ce08f483fc8149b93bd0a93
Vladimir Poliakov <voledemar@inbox.ru>
2018-04-13 19:34:09 +0300

    [add]: base context realizattion #5

---
+++ context/context.go
@@ -0,0 +1,38 @@
+// Copyright 2018 Vladimir Poliakov, Dmitrii Chebaksarov.
+// All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package context
+
+import (
+	goctx "context"
+	"log"
+)
+
+type key int
+
+// Context keys.
+const (
+	KeyDB key = iota
+)
+
+// TODO: change `interface{}` to DB struct when realized.
+
+// New initializes base context.
+func New(db interface{}) goctx.Context {
+	ctx := goctx.Background()
+	ctx = goctx.WithValue(ctx, KeyDB, db)
+
+	return ctx
+}
+
+// DB returns db instance by ctx.
+func DB(ctx goctx.Context) interface{} {
+	db, ok := ctx.Value(KeyDB).(interface{})
+	if !ok {
+		log.Fatalf("context: has no db")
+	}
+
+	return db
+}
