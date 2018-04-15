b6d06b5ced1f034b3600e28b8bdcf84217aa379d
Vladimir Poliakov <voledemar@inbox.ru>
2018-04-15 15:44:22 +0300

    [imp]: #2 add license preamble

--- dto/doc.go
+++ dto/doc.go
@@ -1,3 +1,7 @@
+// Copyright 2018 Vladimir Poliakov and The Contributors. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
 // Package dto provides data containers (DTO pattern)
 // to transfer information between server and DB.
 package dto
