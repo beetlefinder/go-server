7884b61c27aa4ec0caa7d2256735cd47d97734cf
Vladimir Poliakov <voledemar@inbox.ru>
2018-04-18 21:05:47 +0300

    [add]: user controller file

---
+++ controller/user.go
@@ -0,0 +1,9 @@
+// Copyright 2018 Vladimir Poliakov and The Contributors. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package controller
+
+// User is a user controller - handleres served http request
+// in context of user changing / getting.
+type User struct{}
