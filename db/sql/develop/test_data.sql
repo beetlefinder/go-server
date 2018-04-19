1606e4f5d37e55431c6c554c9f44ff335c868d8a
Vladimir Poliakov <voledemar@inbox.ru>
2018-04-19 19:23:28 +0300

    #13 new test data for auth table

--- db/sql/develop/test_data.sql
+++ db/sql/develop/test_data.sql
@@ -6,3 +6,8 @@ INSERT INTO public.user (nick) VALUES
 ('voledemar'),
 ('test_2'),
 ('test_3');
+
+INSERT INTO public.auth (login, pass_hash) VALUES
+('voledemar', '1234hashMD5loool'),
+('test_2_login', 'ergsrthbr8muJMGae85g'),
+('my_login', 'rwagw3t4tfr');
